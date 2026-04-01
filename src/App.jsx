dc3b32ad6b783a184a6e22136648b504e4c59788
Palash-4 <hasanalltarek9658@gmail.com>
2026-04-01 13:28:13 +0600

    navber cart length added

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar carts={carts} ></Navbar>
       <Banner></Banner>
  
       <CardHeading></CardHeading>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = ({carts}) => {
     return (
-        <div className="navbar bg-base-100 shadow-sm px-6 flex justify-around">
+        <div className="navbar sticky top-0 z-50 bg-base-100 shadow-sm px-6 flex justify-around">
             <h1 className="text-3xl text-[#801ff8] font-bold">DigiTools</h1>
 
             <div className="hidden lg:flex gap-6 font-medium text-gray-800">
@@ -21,11 +21,11 @@ const Navbar = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
-                        <span className="badge badge-sm indicator-item">8</span>
+                        <span className="badge badge-sm indicator-item">{carts.length}</span>
                     </div>
                 </div>
                 <button className="btn btn-ghost text-sm">Login</button>
-                <button className="btn bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full px-5 text-xl">
+                <button className="btn bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full px-5 text-[15px]">
                     Get Started
                 </button>
 
